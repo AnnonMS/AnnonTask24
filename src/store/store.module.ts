bf3191fb40452f7baf8f88fa27305c59bf23aa79
Mikołaj <mikolaj.serwicki@gmail.com>
2019-08-23 01:29:01 +0200

    Add Minor tweeks (tslint rule, etc.)

--- src/store/store.module.ts
+++ src/store/store.module.ts
@@ -1,8 +1,8 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { NgxsModule } from '@ngxs/store';
-import { NgxsLoggerPluginModule } from '@ngxs/logger-plugin';
+import { NgModule } from '@angular/core';
 import { NgxsReduxDevtoolsPluginModule } from '@ngxs/devtools-plugin';
+import { NgxsLoggerPluginModule } from '@ngxs/logger-plugin';
+import { NgxsModule } from '@ngxs/store';
 import { DEVTOOLS_REDUX_CONFIG, LOGGER_CONFIG, OPTIONS_CONFIG, STATES_MODULES } from './store.config';
 
 @NgModule({
@@ -14,4 +14,4 @@ import { DEVTOOLS_REDUX_CONFIG, LOGGER_CONFIG, OPTIONS_CONFIG, STATES_MODULES } 
   ],
   exports: [NgxsModule]
 })
-export class NgxsStoreModule {}
+export class NgxsStoreModule { }
